cab5d37b66141f0cf157b30958fc643e464188a6
Shreyas Potnis <shreyaspotnis@gmail.com>
2015-01-08 12:47:35 -0600

    Made python stuff into a package so that it can be used everywhere.

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gpe3d_mpi_nlt',
+      version='0.1',
+      description='Provides methods to analyze 3D GPE simulations run using gpe3d_nlt_mpi',
+      url='https://github.com/shreyaspotnis/gpe3d_mpi_nlt',
+      author='Shreyas Potnis',
+      author_email='shreyaspotnis@gmail.com',
+      license='MIT',
+      packages=['gpe3d_mpi_nlt'],
+      zip_safe=False)
